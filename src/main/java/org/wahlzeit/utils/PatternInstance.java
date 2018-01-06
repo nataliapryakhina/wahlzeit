7e6756bc8e66fc06455ef680e99c5fa33914b23f
nataliapryakhina <npryakhina@gmail.com>
2018-01-06 02:35:24 +0100

    deleted unneeded superwarning

--- src/main/java/org/wahlzeit/utils/PatternInstance.java
+++ src/main/java/org/wahlzeit/utils/PatternInstance.java
@@ -3,7 +3,6 @@ package org.wahlzeit.utils;
 //https://docs.oracle.com/javase/tutorial/java/annotations/declaring.html
 public @interface PatternInstance {
 	String patternName();
-	@SuppressWarnings("rawtypes")
 	String[] participants();
 
 }
