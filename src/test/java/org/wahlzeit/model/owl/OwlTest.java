b2084a016b27f28e96c99d3f3f7e0ff0fd7b802e
nataliapryakhina <npryakhina@gmail.com>
2018-01-15 09:45:31 +0100

    added some tests

--- src/test/java/org/wahlzeit/model/owl/OwlTest.java
+++ src/test/java/org/wahlzeit/model/owl/OwlTest.java
@@ -1,11 +1,24 @@
 package org.wahlzeit.model.owl;
-import org.junit.Test; 
+import org.junit.Assert;
+import org.junit.Test;
+import org.wahlzeit.model.owl.Owl.Color; 
 
 public class OwlTest {
 	@Test
 	public void testOwl(){
 		OwlType polarowl = new OwlType("polar_owl");
+		int age = 4;
+		String name = "Hedwig";
+		Color color = Color.WHITE;
 		Owl harryPotterOwl = new Owl(polarowl);
+		harryPotterOwl.setAge(age);
+		harryPotterOwl.setName(name);
+		harryPotterOwl.setColor(color);
+		
+		Assert.assertEquals(harryPotterOwl.getAge(), age);
+		Assert.assertEquals(harryPotterOwl.getName(), name);
+		Assert.assertEquals(harryPotterOwl.getColor(), color);
+		
 	}
 	
 }
--- src/test/java/org/wahlzeit/model/owl/OwlTypeTest.java
+++ src/test/java/org/wahlzeit/model/owl/OwlTypeTest.java
@@ -1,10 +1,35 @@
 package org.wahlzeit.model.owl;
-import org.junit.Test; 
+import org.junit.Test;
+
+import org.junit.Assert;
 
 public class OwlTypeTest {
+	
+
 	@Test
-	public void testOwl(){
+	public void superTypeTest(){
 		OwlType polarowl = new OwlType("polar_owl");
-		Owl harryPotterOwl = new Owl(polarowl);
+		OwlType harry_poter_owl = new OwlType("harry_poter_owl");
+		harry_poter_owl.setSuperType(polarowl);
+		Assert.assertEquals(harry_poter_owl.getSuperType(), polarowl);
+		
+	}
+	@Test
+	public void subTypeTest(){
+		//super
+		OwlType polarowl = new OwlType("polar_owl");
+		//subtypes
+		OwlType sub_owl1 = new OwlType("owl_1");
+		OwlType sub_owl2 = new OwlType("owl_2");
+		OwlType sub_owl3 = new OwlType("owl_3");
+		//other type
+		OwlType other_owl_type = new OwlType("other_owl_type");
+		polarowl.addSubType(sub_owl1);
+		polarowl.addSubType(sub_owl2);
+		polarowl.addSubType(sub_owl3);
+		
+		Assert.assertTrue(polarowl.isSubType(sub_owl1));
+		Assert.assertFalse(polarowl.isSubType(other_owl_type));
+		
 	}
 }
